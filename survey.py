3e7c97bae91efcc1ef979090ee8164901f5801d7
Siddarthbn <siddarth@vtaraenergygroup.com>
2025-09-18 14:34:31 +0530

    Update survey.py

--- survey.py
+++ survey.py
@@ -47,7 +47,7 @@ def survey(navigate_to, user_email, secrets):
 
     # --- Paths & Constants ---
     bg_path = "images/background.jpg"
-    logo_path = "images/vtara.png"
+    logo_path = "images/VTARA.png"
     MIN_RESPONDENTS_FOR_TEAM_AVERAGE = 1
     TEAM_AVERAGE_DATA_WINDOW_DAYS = 90
 
